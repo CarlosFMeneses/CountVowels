8353911e6c4aaef26fb1181f1ed2ce52b9d8237f
Carlos F. Meneses <CarlosFMeneses@users.noreply.github.com>
2019-04-06 22:23:29 -0400

    Added CountVowels.java

---
+++ countVowels/CountVowels.java
@@ -0,0 +1,61 @@
+/* CountVowels
+ * 
+ * Enter a string and the program counts the number of vowels in the text. 
+ * Also, reports the sum of each vowel found.
+ * 
+ * by Carlos F. Meneses
+ * Created: 04/06/2019
+ * Modified: 04/06/2019
+ * 
+ */
+package countVowels;
+
+/**
+ * The Class CountVowels.
+ */
+public class CountVowels {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		String userString = getUserString("Enter text: ");
+		Object VowelCount = countVowels(userString);
+		display(VowelCount);
+		System.exit(0);
+
+	}
+
+	/**
+	 * Displays instruction and gets user input.
+	 *
+	 * @param string instruction to display
+	 * @return the user string
+	 */
+	private static String getUserString(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @param userString
+	 * @return
+	 */
+	private static Object countVowels(String userString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Display.
+	 *
+	 * @param vowelCount the vowel count
+	 */
+	private static void display(Object vowelCount) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
